fa7fb4287d776156f3c86644f262c19c51fb74c4
Rishitha-09 <84839948+Rishitha-09@users.noreply.github.com>
2021-11-23 17:24:25 +0530

    updates symbols

--- polling-app-server/src/main/java/com/example/polls/config/AuditingConfig.java
+++ polling-app-server/src/main/java/com/example/polls/config/AuditingConfig.java
@@ -1,6 +1,6 @@
 package com.example.polls.config;
 
-import com.example.polls.security.UserPrincipal;
+##import com.example.polls.security.UserPrincipal;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
